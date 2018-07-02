bc37e2dda4212ce99b6ad2d1e675860b52122b13
derrick <kang.derrick@gmail.com>
2018-07-02 20:34:36 +0900

    basic commit

---
+++ app/src/main/java/io/github/superbderrick/summerplayer/ui/LocalFragment.kt
@@ -0,0 +1,19 @@
+package io.github.superbderrick.summerplayer.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.superbderrick.summerplayer.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LocalFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_first, container, false)
+    }
+
+}
---
+++ app/src/main/java/io/github/superbderrick/summerplayer/ui/MainPageAdapter.kt
@@ -0,0 +1,36 @@
+package io.github.superbderrick.summerplayer.ui
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MainPageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                LocalFragment()
+            }
+            1 -> StreamingFragment()
+            2 -> RecentlyFragment()
+            else -> {
+                return LikeFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 4
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "Local File"
+            1 -> "Streaming"
+            2 -> "Recently"
+            else -> {
+                return "Like"
+            }
+        }
+    }
+}
